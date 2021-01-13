26404f84ed3c18d2b04ee8fe02af2673f3393fed
nattaphong2540 <nattaphong.p@kkumail.com>
2021-01-13 18:32:58 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,102 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-row>
+      <b-col cols="12">
+        <b-row class="justify-content-md-center">
+          <b-col cols="12" class="text-center">
+            <h1>TODO LIST V2</h1>
+          </b-col>
+        </b-row>
+      </b-col>
+      <b-col cols="12">
+        <b-row class="justify-content-md-center">
+          <b-col cols="8">
+            <todo-list-viewer
+              ref="todo-component"
+              :todolist="todolist"
+              @removeTodo="removeTodo"
+              @updateCompleted="updateCompleted"
+            />
+          </b-col>
+        </b-row>
+      </b-col>
+      <b-col cols="12">
+        <b-row class="justify-content-md-center">
+          <b-col cols="8">
+           <b-input-group prepend="title" class="mt-3">
+              <b-form-input v-model="title"></b-form-input>
+              <b-input-group-append>
+                <b-button variant="success" @click="addTodo"
+                  >Add</b-button
+                >
+              </b-input-group-append>
+            </b-input-group>
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import TodoListViewer from "./components/TodoListViewer";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TodoListViewer,
+  },
+  created() {
+    this.loadTodo();
+  },
+  data: function () {
+    return {
+      title: "",
+      todolist: [],
+    };
+  },
+  methods: {
+    loadTodo() {
+      axios
+        .get("https://arcane-hollows-66380.herokuapp.com/todos")
+        .then(({ data }) => {
+          this.todolist = data;
+        });
+    },
+    addTodo() {
+      const data = {
+        username: "Thomas",
+        title: this.title,
+      };
+      axios
+        .post(`https://arcane-hollows-66380.herokuapp.com/todos`, data)
+        .then(() => {
+          this.loadTodo();
+        });
+      this.title = "";
+    },
+    removeTodo(id) {
+      axios
+        .delete(`https://arcane-hollows-66380.herokuapp.com/todos/${id}`)
+        .then(() => {
+          this.loadTodo();
+        });
+    },
+    updateCompleted(id, completed) {
+      let updateData = {
+        completed,
+      };
+      axios
+        .put(
+          `https://arcane-hollows-66380.herokuapp.com/todos/${id}`,
+          updateData
+        )
+        .then(() => {
+          this.loadTodo();
+        });
+    },
+  },
 }
 </script>
 
---
+++ src/components/TodoListViewer.vue
@@ -0,0 +1,40 @@
+<template>
+    <b-list-group>
+        <b-list-group-item class="d-flex justify-content-between align-items-center" v-for="(todo, index) in todolist" :key="index">
+            <div>
+                <input
+                    type="checkbox"
+                    name="todo"
+                    v-model="todo.completed"
+                    @change="updateCompleted(todo.id, todo.completed)"
+                />
+                {{ todo.title }}
+            </div>
+            <div>
+                <b-button pill variant="danger" @click="removeTodo(todo.id)">x</b-button>
+            </div>
+        </b-list-group-item>
+    </b-list-group>
+</template>
+
+<script>
+export default {
+  name: "todo-list-viewer",
+  props: {
+    todolist: Array,
+  },
+  components: {},
+  created() {},
+  data: function () {
+    return {};
+  },
+  methods: {
+    removeTodo(id) {
+      this.$emit("removeTodo", id);
+    },
+    updateCompleted(id, completed) {
+      this.$emit("updateCompleted", id, completed);
+    },
+  },
+};
+</script>
